2dbdabb3b4e6743e452ad113776bbfe4261d0b20
Valentosik <46522022+Valentosik@users.noreply.github.com>
2020-02-17 01:48:11 +0300

    Initi commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,22 +1,71 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.domain.Role;
 import com.example.demo.domain.User;
+import com.example.demo.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')") // Аннотация для каждого метода будет проверять наличие прав
 public class UserController {
-    @GetMapping("/user")
-    public String getUserInfo(Map<String, Object> model) {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping()
+    public String getUserInfo(Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        model.put("user", user);
+
+        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("user", user);
+
+        System.out.println(user.getRoles());
 
         return "user";
+    }
+
+    @GetMapping("{user}") // ожидаем еще пользователя
+    public String userEditForm(@PathVariable User user, Model model) { // при переходе не страницу мы получаем шаблон пользователя
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user
+    ) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key: form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+
+        return "redirect:/user";
     }
 
 
--- src/main/java/com/example/demo/repos/DishesRepo.java
+++ src/main/java/com/example/demo/repos/DishesRepo.java
@@ -1,6 +1,10 @@
 package com.example.demo.repos;
 
 import com.example.demo.domain.Dishes;
+import com.example.demo.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +14,7 @@ import java.util.List;
 
 
 @Repository
-public interface DishesRepo extends CrudRepository<Dishes, Long> {
+public interface DishesRepo extends JpaRepository<Dishes, Long> {
     List<Dishes> findAll();
 
     List<Dishes> findOneById(Long id);
@@ -19,7 +23,11 @@ public interface DishesRepo extends CrudRepository<Dishes, Long> {
 
     List<Dishes> findOneByName(String name);
 
-    //Iterable<Dishes> findByUser_id(String user_id);
+    //List<Dishes> findAllByUserId(Long user_id, User user);
+
+    //List<Dishes> findAllByIdAndUserId(Long id, Long user_id);
+
+    //Page<Dishes> findByUserId(Long userId, Pageable pageable);
 
     //@Query("SELECT * FROM public.dishes WHERE user_id=:user_id")
     //Iterable<Dishes> findByUser_id(@Param("user_id") String user_id);
